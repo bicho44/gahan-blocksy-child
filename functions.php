2c2e37c00c1c1fcc1699a059c648d815f8fec9d4
Federico Reinoso <bicho44@gmail.com>
2021-07-19 17:14:38 -0300

    Suspensión temporaria de la CPT de restaurant

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php
-if (! defined('WP_DEBUG')) {
-	die( 'Direct access forbidden.' );
+if (!defined('WP_DEBUG')) {
+	die('Direct access forbidden.');
 }
-add_action( 'wp_enqueue_scripts', function () {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+add_action('wp_enqueue_scripts', function () {
+	wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
 });
 
 /**
  * Load IMGD Framework compatibility file.
  */
-get_template_part('imgd/imgd','CPT_Gahan');
+// get_template_part('imgd/imgd','CPT_Gahan');
